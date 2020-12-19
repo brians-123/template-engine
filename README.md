29578528d4f2178b9fd7bc93e49672b169a24627
brians-123 <brian_shor@hotmail.com>
2020-12-18 22:05:25 -0800

    minor formatting changes

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# template-engine
-Node CLI application that builds HTML webpage for employee information
+# Team Template Engine
+
+## Table of Contents :book:
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribute](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description <a id="description"></a> :page_facing_up:
+
+Node CLI application that builds HTML webpage for a manager's employee
+This project uses node.js and the inquirer npm package to prompt a user with questions and collect their answers. The answers are then used to generate an html file as a visual representation of their team.
+
+Jest tests were provided for this project to demonstrate test driven development. The process to compelete this project was to run the existing test classes, make changes to the employee classes to get them working, then begin on developing the app functionality.
+
+An example html document generated through this application 'team.html' is contained within the output folder. When the application is run, it will overwrite the file.
+
+Here is a [demo](https://drive.google.com/file/d/1vs1ipBXnDzJ_MHrs7N2eY_ujZSpHriEF/view)
+
+## Installation <a id="installation"></a> :floppy_disk:
+
+- Install the LTS version of node https://nodejs.org/en/
+- Download this repository from github
+- Go to the terminal and type 'install npm' then hit enter
+- Wait for the installation to complete then hit enter
+- Type npm i inquirer then hit enter
+- Wait for the installation to complete then hit enter
+
+## Usage <a id="usage"></a> :computer:
+
+- This program must be run in your terminal with the following command:
+
+  ```bash
+  node index.js
+  ```
+
+- Run through the list of questions hitting enter after each answer
+
+## License <a id="license"></a> :copyright:
+
+This application should not be modified or distributed
+
+## Contribute <a id="contribute"></a> :heavy_plus_sign: :heavy_plus_sign:
+
+Please do not contribute to this application
+
+## Tests <a id="tests"></a> :microscope:
+
+Jest tests were provided for this project to demonstrate test driven development. The process to compelete this project was to run the existing test classes, make changes to the employee classes to get them working, then begin on developing the app functionality.
+
+## Contact <a id="questions"></a> :email:
+
+[github account](https://github.com/brians-123)
+not sharing
--- app.js
+++ app.js
@@ -9,25 +9,13 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 //the first role entered is assumed to be a manager
 let role = "Manager";
-
+//this array will store our employee objects
 const employees = [];
-//Ask for manager info
-// function askMgrQuestions (){
 
-// }
-
-// Name?
-// Id?
-// email?
-// Role?
-//   dependent on what's give, office, git, school
-// Add another?
-
-//add general questions
-const genericQuestions = [
+//general questions
+const empQuestions = [
   {
     type: "input",
     message: "What is the employee's name?",
@@ -43,15 +31,12 @@ const genericQuestions = [
     message: "What is the employee's email?",
     name: "email",
   },
-
-  //starting with the manager's specific question. we will replace this for subsequent questions.
+  //Start with manager specific question. We replace with a new question for each role
   {
     type: "input",
     message: "What is the employee's office number?",
     name: "officeNumber",
   },
-
-  //not sure if there will be an impact here due to too many arguments being passed in. I may have to rework after trying html
   {
     type: "list",
     message: "Which type of team member would you like to add next?",
@@ -62,20 +47,9 @@ const genericQuestions = [
 
 //role specific questions
 function askRoleSpecificQuestion(theNextRole) {
-  // var specificQuestion = {};
   switch (theNextRole) {
-    // case "Manager":
-    //   console.log("it's a manager");
-    //   genericQuestions[3] = {
-    //     type: "input",
-    //     message: "What is the employee's office number?",
-    //     name: "officeNumber",
-    //   };
-    //   break;
-
     case "Engineer":
-      console.log("it's an engineer");
-      genericQuestions[3] = {
+      empQuestions[3] = {
         type: "input",
         message: "What is the employee's github id?",
         name: "github",
@@ -83,8 +57,7 @@ function askRoleSpecificQuestion(theNextRole) {
       break;
 
     case "Intern":
-      console.log("it's an intern");
-      genericQuestions[3] = {
+      empQuestions[3] = {
         type: "input",
         message: "What is the employee's school?",
         name: "school",
@@ -93,145 +66,49 @@ function askRoleSpecificQuestion(theNextRole) {
   }
 }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// function askEngQuestions (){
-
-// }
-const engQuestions = [
-  {
-    type: "input",
-    message: "What is your engineer's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is engineer's id?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your engineer's email?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your engineer's github username?",
-    name: "role",
-  },
-];
-
-//intern questions
-// function askInternQuestions (){
-
-// }
-//can i write a function inside the object for the last question? It seems like I can
-
-const intQuestions = [
-  {
-    type: "input",
-    message: "What is your intern's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is intern's id?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your intern's email?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your intern's school?",
-    name: "school",
-  },
-];
+function createEmployee(data) {
+  switch (data.role) {
+    case "Manager":
+      employee = new Manager(data.name, data.id, data.email, data.officeNumber);
+      break;
+    case "Engineer":
+      employee = new Engineer(data.name, data.id, data.email, data.github);
+      break;
+    case "Intern":
+      employee = new Intern(data.name, data.id, data.email, data.school);
+      break;
+  }
+}
 
 //start inquirer
 function start() {
   inquirer
-    //for the first run, we know the role is manager
-    //we will get the role for the next run, prior to this being run
-    //save that in a variable that starts as manager
-    .prompt(genericQuestions)
+    //for the first run, we know the role is manager. Subsequent runs
+    //replace this question
+    .prompt(empQuestions)
 
     .then((data) => {
-      //we can assume the first employee will be the manager
+      //we can assume the first employee will be the manager and we set it in the object
       data["role"] = role;
 
-      console.log(data);
-      //use a switch case to determine the last question
-
       askRoleSpecificQuestion(data.nextRole);
-      //placeholder - this is where I removed the creation of a new employee
-
-      switch (role) {
-        case "Manager":
-          employee = new Manager(
-            data.name,
-            data.id,
-            data.email,
-            data.officeNumber
-          );
-          console.log("we made a manager");
-          break;
-        case "Engineer":
-          employee = new Engineer(data.name, data.id, data.email, data.github);
-          console.log("we made an engineer");
-          break;
-        case "Intern":
-          employee = new Intern(data.name, data.id, data.email, data.school);
-          console.log("we made an intern");
-          break;
-      }
-
+      createEmployee(data);
       employees.push(employee);
       if (data.nextRole === `I don't want more team members`) {
         const renderStuff = render(employees);
-        console.log(employees);
         fs.writeFile("./output/team.html", renderStuff, "utf8", (err) =>
           err
             ? console.log(err)
             : console.log("You have successfully generated a new team site!")
         );
       } else {
-        console.log("here's the next role" + data.nextRole);
+        //we asked for the next role in inquirer and set it for the next render
         role = data.nextRole;
-        //we need to ask questions about the next employee
+        //we start the process over again for the next employee
         start();
       }
-      // employee = new Manager(data.name, data.id, data.email, data.officeNumber);
-      // After the user has input all employees desired, call the `render` function (required
-      // above) and pass in an array containing all employee objects; the `render` function will
-      // generate and return a block of HTML including templated divs for each employee!
-      // employees.push(employee);
-      // const renderStuff = render(employees);
-      // fs.writeFile("./output/team.html", renderStuff, "utf8", (err) =>
-      // err
-      // ? console.log(err)
-      // : console.log("You have successfully generated a new team site!")
-      // );
     });
 }
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 console.log("Please enter your information first");
 start();
